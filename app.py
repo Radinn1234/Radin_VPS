064e6823bd90ef709afe72e49efa2aef3a72f072
Radinn1234 <radin.hashemi14@gmail.com>
2026-08-26 12:23:21 +0330

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return '<h1>سلام! این سایت داخل GitHub Actions ران شده 🎉</h1>'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
